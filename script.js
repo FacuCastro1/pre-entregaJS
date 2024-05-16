58a4710e9d9cb04299b19fb97e43e96f0863530c
FacuCastro1 <facundocastrom99@gmail.com>
2024-05-15 21:49:57 -0400

    todo renovado

--- script.js
+++ script.js
@@ -1,122 +1,152 @@
-// Mensaje de bienvenida
-alert("¡Bienvenido a La Cabra Jerseys!");
-
-// Defino mis camisetas de fútbol
-const camisetas = [
-    { equipo: "Real Madrid", precio: 50, stock: 3, rutaImagen: "real-madrid.webp" },
-    { equipo: "Barcelona", precio: 45, stock: 8, rutaImagen: "barcelona-fc.webp" },
-    { equipo: "Manchester United", precio: 40, stock: 12, rutaImagen: "man-united.jpeg" },
-    { equipo: "Juventus", precio: 55, stock: 6, rutaImagen: "juventus.png" },
-    { equipo: "Bayern Munich", precio: 60, stock: 7, rutaImagen: "bayern.webp" },
-    { equipo: "Liverpool", precio: 50, stock: 1, rutaImagen: "liverpool.webp" },
-    { equipo: "Paris Saint-Germain", precio: 55, stock: 5, rutaImagen: "psg.jpeg" },
-    { equipo: "Manchester City", precio: 45, stock: 10, rutaImagen: "man-city.jpeg" }
+// Definimos nuestros productos
+let listaProductos = [
+    {id: 1, equipo: "Real Madrid", precio: 50, stock: 3, rutaImagen: "real-madrid.webp" },
+    {id: 2, equipo: "Barcelona", precio: 45, stock: 8, rutaImagen: "barcelona-fc.webp" },
+    {id: 3, equipo: "Manchester United", precio: 40, stock: 12, rutaImagen: "man-united.jpeg" },
+    {id: 4, equipo: "Juventus", precio: 55, stock: 6, rutaImagen: "juventus.png" },
+    {id: 5, equipo: "Bayern Munich", precio: 60, stock: 7, rutaImagen: "bayern.webp" },
+    {id: 6, equipo: "Liverpool", precio: 50, stock: 1, rutaImagen: "liverpool.webp" },
+    {id: 7, equipo: "Paris Saint-Germain", precio: 55, stock: 5, rutaImagen: "psg.jpeg" },
+    {id: 8, equipo: "Manchester City", precio: 45, stock: 10, rutaImagen: "man-city.webp" }
 ];
 
-// Objeto carrito de compras
-const carrito = {
-    camisetas: [],
-    total: 0
-};
+// Variables
+const contenedorProductos = document.getElementById("contenedorProductos");
+const listaCarrito = document.getElementById("listaCarrito");
+let carrito = [];
 
-// Función para filtrar camisetas por precio igual o inferior a cierto valor
-function filtrarPorPrecioMaximo(precioMaximo) {
-    return camisetas.filter(camiseta => camiseta.precio <= precioMaximo);
+// Almacenamiento local (carrito)
+function guardarCarritoEnStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
-// Función para mostrar la información de los objetos
-function mostrarInfoCamisetas() {
-    let mensaje = "Ingrese el equipo que desea:\n\n";
-    camisetas.forEach((camiseta) => {
-        mensaje += `${camiseta.equipo} - $${camiseta.precio} - Stock: ${camiseta.stock}\n`;
+// Cargar el carrito desde el almacenamiento local
+function cargarCarritoDesdeStorage() {
+    const carritoGuardado = localStorage.getItem('carrito');
+    if (carritoGuardado) {
+        carrito = JSON.parse(carritoGuardado);
+        mostrarCarrito();
+    }
+}
+
+// Agregar un producto al carrito
+function agregarAlCarrito(id) {
+    const producto = listaProductos.find(item => item.id === id);
+    carrito = producto ? [...carrito, producto] : carrito;
+    mostrarCarrito();
+}
+
+function agregarAlCarrito(id) {
+    const productoExistenteIndex = carrito.findIndex(item => item.id === id); // Buscamos el índice del producto en el carrito
+    if (productoExistenteIndex !== -1) {
+        // Si el producto ya está en el carrito, actualizamos su cantidad y precio total
+        carrito[productoExistenteIndex].cantidad++;
+        carrito[productoExistenteIndex].precioTotal = carrito[productoExistenteIndex].cantidad * carrito[productoExistenteIndex].precio;
+    } else {
+        // Si el producto no está en el carrito, lo agregamos al carrito con una cantidad inicial de 1
+        const producto = listaProductos.find(item => item.id === id);
+        carrito.push({...producto, cantidad: 1, precioTotal: producto.precio});
+    }
+    mostrarCarrito(); // Mostramos el carrito actualizado
+}
+
+function mostrarCarrito() {
+    listaCarrito.innerHTML = `
+        <thead>
+            <tr>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+    `;
+    let totalCarrito = 0;
+    carrito.forEach(({ id, equipo, precio, cantidad, precioTotal }) => {
+        totalCarrito += precioTotal;
+        listaCarrito.innerHTML += `
+            <tr>
+                <td>${equipo}</td>
+                <td>$${precio}</td>
+                <td>
+                    <button onclick="disminuirCantidad(${id})">-</button>
+                    ${cantidad}
+                    <button onclick="aumentarCantidad(${id})">+</button>
+                </td>
+                <td>$${precioTotal}</td>
+                <td><button onclick="eliminarProductoDelCarrito(${id})">Eliminar</button></td>
+            </tr>
+        `;
     });
-    return mensaje;
+    const totalCarritoElement = document.getElementById("totalCarrito");
+    totalCarritoElement.textContent = `Total: $${totalCarrito}`;
 }
 
-// Función para agregar camiseta al carrito
-function agregarAlCarrito(equipo, cantidad) {
-    const equipoLowerCase = equipo.toLowerCase(); // Convertir entrada del usuario a minúsculas
+function aumentarCantidad(id) {
+    const producto = carrito.find(item => item.id === id);
+    producto.cantidad++;
+    producto.precioTotal = producto.cantidad * producto.precio;
+    mostrarCarrito();
+}
 
-    const camisetaEnStock = camisetas.find(item => item.equipo.toLowerCase() === equipoLowerCase);
-
-    if (camisetaEnStock && camisetaEnStock.stock >= cantidad) {
-        carrito.camisetas.push({ equipo: camisetaEnStock.equipo, precio: camisetaEnStock.precio, cantidad });
-        camisetaEnStock.stock -= cantidad;
-        alert(`¡Camiseta del ${camisetaEnStock.equipo} agregada al carrito!`);
-    } else if (camisetaEnStock) {
-        alert(`Lo sentimos, la camiseta del ${camisetaEnStock.equipo} no está disponible en la cantidad solicitada.`);
+function disminuirCantidad(id) {
+    const producto = carrito.find(item => item.id === id);
+    if (producto.cantidad > 1) {
+        producto.cantidad--;
+        producto.precioTotal = producto.cantidad * producto.precio;
     } else {
-        alert(`Lo sentimos, la opción "${equipo}" no está disponible en este momento.`);
+        eliminarProductoDelCarrito(id); // Si la cantidad es 1 o menos, eliminamos el producto del carrito
     }
+    mostrarCarrito();
 }
 
-// Función para calcular el total de la compra
-function calcularTotal() {
-    carrito.total = carrito.camisetas.reduce((total, camiseta) => total + (camiseta.precio * camiseta.cantidad), 0);
+function eliminarProductoDelCarrito(id) {
+    carrito = carrito.filter(item => item.id !== id); // Filtramos el carrito para eliminar el producto con el ID dado
+    mostrarCarrito(); // Volvemos a mostrar el carrito actualizado
 }
 
-// Función para procesar el pago
-function procesarPago() {
-    calcularTotal();
-    const confirmacion = confirm(`El total de la compra es $${carrito.total}. ¿Desea proceder con el pago?`);
 
-    if (confirmacion) {
-        const nombre = prompt("Ingrese su nombre:");
-        const tarjeta = prompt("Ingrese el número de su tarjeta de crédito:");
-
-        if (nombre && tarjeta) {
-            alert(`¡Gracias por su compra, ${nombre}! El pago de $${carrito.total} ha sido procesado con éxito.`);
-        } else {
-            alert("Por favor, complete todos los campos para procesar el pago.");
-        }
-    } else {
-        alert("¡Gracias por visitarnos! Esperamos verte de nuevo pronto.");
-    }
+// Creando las tarjetas del producto
+function crearTarjetasDeProductos(productos) {
+    contenedorProductos.innerHTML = "";
+    productos.forEach(producto => {
+        const tarjetaProducto = document.createElement("div");
+        tarjetaProducto.innerHTML = `
+            <img src="./camisetas/${producto.rutaImagen}" />
+            <h3>${producto.equipo}</h3>
+            <h4>$ ${producto.precio}</h4>
+            <button class="agregar-carrito-btn" onclick="agregarAlCarrito(${producto.id})">AGREGAR</button>
+        `;
+        contenedorProductos.appendChild(tarjetaProducto);
+    });
 }
 
-// Interfaz de usuario
-function iniciarSimulador() {
-    let continuar = true;
-
-    while (continuar) {
-        const opcion = prompt(`Seleccione una opción:
-        1. Agregar una camiseta al carrito
-        2. Proceder al pago
-        3. Salir`);
-
-        switch (opcion) {
-            case "1":
-                const equipo = prompt(mostrarInfoCamisetas());
-                const camisetaEnStock = camisetas.find(item => item.equipo.toLowerCase() === equipo.toLowerCase());
-
-                if (camisetaEnStock && camisetaEnStock.stock >= 1) {
-                    const cantidad = parseInt(prompt("Ingrese la cantidad:"));
-
-                    if (!cantidad || cantidad <= 0) {
-                        alert("Por favor, ingrese una cantidad válida.");
-                        continue;
-                    }
-
-                    agregarAlCarrito(equipo, cantidad);
-                } else if (camisetaEnStock) {
-                    alert(`Lo sentimos, la camiseta del ${camisetaEnStock.equipo} no está disponible en la cantidad solicitada.`);
-                } else {
-                    alert(`Lo sentimos, la opción "${equipo}" no está disponible en este momento.`);
-                }
-                break;
-            case "2":
-                procesarPago();
-                continuar = false;
-                break;
-            case "3":
-                alert("¡Gracias por visitarnos! Hasta luego.");
-                continuar = false;
-                break;
-            default:
-                alert("Por favor, seleccione una opción válida.");
-        }
-    }
+// Filtrar productos por nombre
+function filtrarProductos() {
+    const textoBusqueda = document.getElementById("buscador").value.toLowerCase();
+    const productosFiltrados = listaProductos.filter(producto => producto.equipo.toLowerCase().includes(textoBusqueda));
+    crearTarjetasDeProductos(productosFiltrados);
 }
 
-// Iniciar el simulador
-iniciarSimulador();
+// Escucha el evento 'input' en el campo de búsqueda
+document.getElementById("buscador").addEventListener("input", filtrarProductos);
+
+// Variable para el botón de alternar el carrito
+const toggleCarritoBtn = document.getElementById("toggleCarrito");
+
+// Variable para el contenido del carrito
+const contenidoCarrito = document.getElementById("contenidoCarrito");
+
+// Escuchar clics en el botón de alternar el carrito
+toggleCarritoBtn.addEventListener("click", () => {
+    // Alternar la visibilidad del contenido del carrito
+    contenidoCarrito.style.display = contenidoCarrito.style.display === "block" ? "none" : "block";
+});
+
+// Llamamos a la función para crear las tarjetas de productos al cargar la página
+window.onload = function() {
+    console.log("La página se ha cargado correctamente.");
+    crearTarjetasDeProductos(listaProductos);
+};
